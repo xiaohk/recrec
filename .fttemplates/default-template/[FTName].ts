ebd1f0bb41b33558f658c5238c3369fbde052ba9
Jay Wang <jay@zijie.wang>
2024-05-23 09:40:16 -0400

    Init commit

---
+++ .fttemplates/default-template/[FTName].ts
@@ -0,0 +1,65 @@
+import { LitElement, css, unsafeCSS, html, PropertyValues } from 'lit';
+import { customElement, property, state, query } from 'lit/decorators.js';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+
+import componentCSS from './<FTName>.css?inline';
+
+/**
+ * [FTName | sentencecase] element.
+ */
+@customElement('jay-[FTName]')
+export class Jay<FTName | pascalcase> extends LitElement {
+  //==========================================================================||
+  //                              Class Properties                            ||
+  //==========================================================================||
+
+  //==========================================================================||
+  //                             Lifecycle Methods                            ||
+  //==========================================================================||
+  constructor() {
+    super();
+  }
+
+  /**
+   * This method is called when the DOM is added for the first time
+   */
+  firstUpdated() {}
+
+  /**
+   * This method is called before new DOM is updated and rendered
+   * @param changedProperties Property that has been changed
+   */
+  willUpdate(changedProperties: PropertyValues<this>) {}
+
+  //==========================================================================||
+  //                              Custom Methods                              ||
+  //==========================================================================||
+  async initData() {};
+
+  //==========================================================================||
+  //                              Event Handlers                              ||
+  //==========================================================================||
+
+  //==========================================================================||
+  //                             Private Helpers                              ||
+  //==========================================================================||
+
+  //==========================================================================||
+  //                           Templates and Styles                           ||
+  //==========================================================================||
+  render() {
+    return html` <div class="<FTName | paramcase>">[FTName | sentencecase]</div> `;
+  };
+
+  static styles = [
+    css`
+      ${unsafeCSS(componentCSS)}
+    `
+  ];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'jay-<FTName>': Jay<FTName | pascalcase>;
+  }
+}
---
+++ src/components/app/app.ts
@@ -0,0 +1,63 @@
+import { LitElement, css, unsafeCSS, html, PropertyValues } from 'lit';
+import { customElement, property, state, query } from 'lit/decorators.js';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+
+import componentCSS from './app.css?inline';
+
+/**
+ * App element.
+ *
+ */
+@customElement('jay-app')
+export class JayApp extends LitElement {
+  //==========================================================================||
+  //                              Class Properties                            ||
+  //==========================================================================||
+
+  //==========================================================================||
+  //                             Lifecycle Methods                            ||
+  //==========================================================================||
+  constructor() {
+    super();
+  }
+
+  /**
+   * This method is called before new DOM is updated and rendered
+   * @param changedProperties Property that has been changed
+   */
+  willUpdate(changedProperties: PropertyValues<this>) {}
+
+  //==========================================================================||
+  //                              Custom Methods                              ||
+  //==========================================================================||
+  async initData() {}
+
+  //==========================================================================||
+  //                              Event Handlers                              ||
+  //==========================================================================||
+
+  //==========================================================================||
+  //                             Private Helpers                              ||
+  //==========================================================================||
+
+  //==========================================================================||
+  //                           Templates and Styles                           ||
+  //==========================================================================||
+  render() {
+    return html`
+      <div class="app">Jay's App, remember to update template folder</div>
+    `;
+  }
+
+  static styles = [
+    css`
+      ${unsafeCSS(componentCSS)}
+    `
+  ];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'jay-app': JayApp;
+  }
+}
